3c03de707c7ba7a257ec807f0eed087778f41af5
Vasily Petuhov <vspetukhov@icloud.com>
2021-09-16 01:29:41 +0300

    done task-1

--- Petuhov_Lesson_2.c
+++ Petuhov_Lesson_2.c
@@ -41,9 +41,63 @@ int main()
     return  0;
 }
 
+// MARK: Menu of the programm
 void menu()
 {
     printf("\n1 — Conversion decimal to binary\n");
     printf("2 — Raising the number `a` to the power `b`\n");
     printf("0 — Exit\n");
+}
+
+
+// MARK: 1. Реализовать функцию перевода из десятичной системы в двоичную, используя рекурсию.
+// этот вариант просто печатает последовательность
+void decimalToBinary(int number)
+{
+    int resultDivision, remainderDivision;
+    
+    resultDivision = number / 2;
+    remainderDivision = number % 2;
+    
+    if (number >= 2) {
+        decimalToBinary(resultDivision);
+    }
+    printf("%d", remainderDivision);
+}
+
+// этот вариант возвращает int значение, для дальнейшего использования
+int decimalToBinary2(int number)
+{
+    int resultDivision, remainderDivision;
+    
+    resultDivision = number / 2;
+    remainderDivision = number % 2;
+    
+    if (number == 0)
+        return 0;
+    else
+        return (decimalToBinary2(resultDivision) * 10 + remainderDivision);
+}
+
+void solution1()
+{
+    int number, result;
+    
+    printf("\nConversion decimal number to binary.\n");
+    
+    printf("Enter decimal number (example: 49): ");
+    scanf("%d", &number);
+    
+    printf("Result by print: ");
+    decimalToBinary(number); // функция сама печатает результат
+    printf("\n");
+    
+    result = decimalToBinary2(number); // функция возвращает значение int
+    printf("Result by return `int`: %d\n", result);
+}
+
+
+// MARK: 2-
+void solution2()
+{
 }
