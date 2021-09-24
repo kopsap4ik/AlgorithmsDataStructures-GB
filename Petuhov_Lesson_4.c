09c9648a0b75692103a95ec90d65cbfdaf788d26
Vasily Petuhov <vspetukhov@icloud.com>
2021-09-25 00:41:04 +0300

    done 2-nd task

--- Petuhov_Lesson_4.c
+++ Petuhov_Lesson_4.c
@@ -7,11 +7,14 @@
 
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
+
+#define RowsMax 6
+#define ColumnsMax 6
 
 void menu();
 void solution1();
 void solution2();
-void solution3();
 
 int main()
 {
@@ -30,9 +33,6 @@ int main()
             case 2:
                 solution2();
                 break;
-            case 3:
-                solution3();
-                break;
             case 0:
                 printf("Finished, Bye!\n");
                 break;
@@ -47,105 +47,157 @@ int main()
 
 void menu()
 {
-    printf("\n1 — Body Mass Index\n");
-    printf("2 — The maximum of four numbers\n");
-    printf("3 — Swap two numbers\n");
+    printf("\n1 — Calculating the number of routes on the map\n");
+    printf("2 — Finding the longest common subsequence\n");
     printf("0 — Exit\n");
 }
 
 
-//1. Ввести вес и рост человека. Рассчитать и вывести индекс массы тела по формуле I=m/(h*h); где m-масса тела в килограммах, h - рост в метрах.
+// MARK: 1. Количество маршрутов с препятствиями. Реализовать чтение массива с препятствием и нахождение количество маршрутов.
+// Например, карта:
+// размер: 3x3
+//  1 1 1
+//  0 1 0
+//  0 1 0
+
+int barriersMap[RowsMax][ColumnsMax] =
+{
+    {1, 1, 1, 1, 1, 1},
+    {0, 1, 1, 0, 1, 1},
+    {0, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 0},
+    {1, 1, 0, 1, 1, 1},
+    {0, 1, 1, 1, 1, 1}
+};
+
+int moveMap[RowsMax][ColumnsMax];
+
+
+void printMap(int numberRows, int numberColumns, int array[RowsMax][ColumnsMax])
+{
+    int row, column;
+    for (row = 0; row < numberRows; row++)
+    {
+        for (column = 0; column < numberColumns; column++)
+        {
+            printf("%6d", array[row][column]);
+        }
+        printf("\n");
+    }
+    
+}
+
+void countRoutes()
+{
+    int row, column;
+    
+    // заполнение 1-го ряда
+    for (column = 0; column < ColumnsMax; column++) {
+        if (barriersMap[0][column] == 0) moveMap[0][column] = 0;
+        else moveMap[0][column] = 1;
+    }
+    
+    // заполнение 1-го колонки
+    for (row = 0; row < ColumnsMax; row++) {
+        if (barriersMap[row][0] == 0) moveMap[row][0] = 0;
+        else moveMap[row][0] = 1;
+    }
+
+    // заполнение оставшейся части
+    for (row = 1; row < RowsMax; row++)
+    {
+        for (column = 1; column < ColumnsMax; column++) {
+            if (barriersMap[row][column] == 1)
+            {
+                moveMap[row][column] = moveMap[row - 1][column] + moveMap[row][column - 1];
+            }
+
+            else moveMap[row][column] = 0;
+
+        }
+    }
+}
+
 void solution1()
 {
-    double m;
-    double h;
+    printf("\nInitial map with barriers (`1` - allow move and `0` - disallow):\n");
+    printMap(RowsMax, ColumnsMax, barriersMap);
     
-    printf("\nBody Mass Index.\n");
+    countRoutes();
     
-    printf("Enter weight in kilograms (example: 75): ");
-    scanf("%lf", &m);
-
-    printf("Enter height in meters (example: 1.75): ");
-    scanf("%lf", &h);
-    
-    double i = m/(h*h);
-
-    printf("Body Mass Index: %.2f\n", i);
+    printf("\nFinal calculation with numbers of routes:\n");
+    printMap(RowsMax, ColumnsMax, moveMap);
 }
 
-// 2. Найти максимальное из четырех чисел. Массивы не использовать.
+
+// MARK: 2. Решить задачу о нахождении длины максимальной последовательности с помощью матрицы.
+// longest common subsequence — наибольшая общая подпоследовательность
+
+int row, column, lengthText1, lengthText2, textsMap[20][20];
+char text1[20], text2[20], directionMap[20][20];
+
+void printMapLCS(int row, int column)
+{
+    if (row == 0 || column == 0) {
+        return;
+    }
+    
+    if (directionMap[row][column] == 'c') {
+        printMapLCS(row - 1, column - 1);
+        printf("%c", text1[row - 1]);
+    } else if (directionMap[row][column] == 'u') {
+        printMapLCS(row - 1, column);
+    } else {
+        printMapLCS(row, column - 1);
+    }
+}
+
+void lcs()
+{
+
+    lengthText1 = strlen(text1);
+    lengthText2 = strlen(text2);
+
+    // заполнение столбца и ряда карты textMaps
+    for (row = 0; row <= lengthText1; row++)
+        textsMap[row][0] = 0;
+    for (column = 0; column <= lengthText2; column++)
+        textsMap[0][column] = 0;
+    
+    // Обозначение вариантов движения в карте directionMap: `c`, `u`, `l` — cross, upper, left
+    for (row = 0; row <= lengthText1; row++) {
+        for (column = 0; column <= lengthText2; column++) {
+            
+            if (text1[row - 1] == text2[column - 1]) {
+                textsMap[row][column] = textsMap[row - 1][column - 1] + 1;
+                directionMap[row][column] = 'c';
+            } else if (textsMap[row - 1][column] >= textsMap[row][column - 1]){
+                textsMap[row][column] = textsMap[row - 1][column];
+                directionMap[row][column] = 'u';
+            } else {
+                textsMap[row][column] = textsMap[row][column - 1];
+                directionMap[row][column] = 'l';
+            }
+            
+        }
+    }
+    
+}
+
 void solution2()
 {
-    int a, b, c, d;
-    int firstCandidate, secondCandidate;
-
-    printf("\nThe maximum of four numbers.\n");
+    printf("\nFinding the longest common subsequence of two sequences.\n");
     
-    printf("Enter 1st number: ");
-    scanf("%i", &a);
+    printf("Enter 1-st sequence of symbols: ");
+    scanf("%s", text1);
     
-    printf("Enter 2nd number: ");
-    scanf("%i", &b);
+    printf("Enter 2-nd sequence of symbols: ");
+    scanf("%s", text2);
     
-    printf("Enter 3rd number: ");
-    scanf("%i", &c);
+    printf("\nThe longest common subsequence from two sequences: ");
+    lcs();
+    printMapLCS(lengthText1, lengthText2);
     
-    printf("Enter 4st number: ");
-    scanf("%i", &d);
+    printf("\n");
     
-    if (a > b) {
-        firstCandidate = a;
-    } else {
-        firstCandidate = b;
-    }
-    
-    if (c > d) {
-        secondCandidate = c;
-    } else {
-        secondCandidate = d;
-    }
-    
-    if (firstCandidate > secondCandidate) {
-        printf("Largest number: %i\n", firstCandidate);
-    } else {
-        printf("Largest number: %i\n", secondCandidate);
-    }
-    
-}
-
-// 3. Написать программу обмена значениями двух целочисленных переменных:
-//  a. с использованием третьей переменной;
-//  b. *без использования третьей переменной.
-void solution3()
-{
-    int a, b;
-    int temp_a;
-    
-    // 1
-    printf("\n1) Swap method with an additional variable.\n");
-    printf("Enter 1st number: ");
-    scanf("%i", &a);
-    
-    printf("Enter 2nd number: ");
-    scanf("%i", &b);
-    
-    temp_a = a;
-    a = b;
-    b = temp_a;
-    
-    printf("Method 1. Swap result: %i and %i\n", a, b);
-    
-    // 2*
-    printf("\n2) Swap method without additional variable.\n");
-    printf("Enter 1st number: ");
-    scanf("%i", &a);
-    
-    printf("Enter 2nd number: ");
-    scanf("%i", &b);
-    
-    a = a + b;
-    b = a - b;
-    a = a - b;
-    
-    printf("Method 2. Swap result: %i and %i\n", a, b);
 }
