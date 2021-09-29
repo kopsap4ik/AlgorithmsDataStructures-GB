1c497062972022b73ffb5b9d9f9098b76958b7b7
Vasily Petuhov <vspetukhov@icloud.com>
2021-09-29 19:11:00 +0300

    done 3 task

--- Petuhov_Lesson_5.c
+++ Petuhov_Lesson_5.c
@@ -9,6 +9,7 @@
 #include <math.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 void menu();
 void solution1();
@@ -112,7 +113,116 @@ void solution2()
 // неправильных: )(, ())({), (, ])}), ([(]) для скобок [,(,{.
 // Примеры входных выражений: (2+(2*2)) или [2/{5*(4+7)}]
 
+typedef struct TNode3 Node3;
+
+struct TNode3 {
+    char value;
+    struct TNode3 *next;
+};
+
+struct Stack3 {
+    Node3 *head;
+    int size3;
+    int maxSize;
+};
+
+struct Stack3 stack3;
+
+void pushToStackChar(char value) {
+    if (stack3.size3 >= stack3.maxSize) {
+        printf("Stack overflow\n");
+        return;
+    }
+    Node3 *tmp = (Node3*)malloc(sizeof(Node3));
+    
+    if (tmp == NULL) {
+        printf("No memory allocated\n");
+    } else {
+        tmp->value = value;
+        tmp->next = stack3.head;
+        
+        stack3.head = tmp;
+        stack3.size3++;
+    }
+}
+
+char popFromStackChar(void) {
+    if (stack3.size3 == 0) return '!';
+    
+    Node3* next = NULL;
+    
+    char value;
+    value = stack3.head->value;
+    next = stack3.head;
+    
+    stack3.head = stack3.head->next;
+    free(next);
+    
+    stack3.size3--;
+    return value;
+}
+
+void printR(Node3* current) {
+    if (current != NULL) {
+        printR(current->next);
+        printf("%i ", current->value);
+    }
+}
+
+int isForwardBracket(char symbol) {
+    if (symbol == '(' || symbol == '[' || symbol == '{') return 1;
+    else return 0;
+}
+
+int isBackwardBracket(char symbol) {
+    if (symbol == ')' || symbol == ']' || symbol == '}') return 1;
+    else return 0;
+}
+
+int isNumber(char *str) {
+    int i = 0;
+    while (str[i] != '\0')
+        if (!isdigit(str[i++])) return 0;
+    return 1;
+}
+
 void solution3()
 {
+    stack3.maxSize = 100;
+    stack3.head = NULL;
+    int backwardIsFirst = 0;
+    char buffer[100];
     
+    printf("Enter a sequence of brackets (for example (2+(2*2))): ");
+    scanf("%s", buffer);
+    
+    for (int i = 0; i < strlen(buffer); i++) {
+        if (isForwardBracket(buffer[i])) {
+            printf("Forward bracket %c is in %i position\n", buffer[i], i+1);
+            pushToStackChar(buffer[i]);
+        }
+        
+        if (isBackwardBracket(buffer[i])) {
+            printf("Backward bracket %c is in %i position\n", buffer[i], i+1);
+            char charFromStack = popFromStackChar();
+            if (charFromStack == '!') {
+                backwardIsFirst = 1;
+                break;
+            }
+            if ((charFromStack == '(' && buffer[i] != ')') ||
+                (charFromStack == '[' && buffer[i] != ']') ||
+                (charFromStack == '{' && buffer[i] != '}')) {
+                printf("Bracket %c in %i position is set incorrectly\n", buffer[i], i+1);
+                break;
+            }
+        }
+    }
+    
+    char lastCharFromStack = popFromStackChar();
+    
+    if (lastCharFromStack != '!') printf("Error: missing closing bracket\n");
+    else {
+        if (backwardIsFirst) printf("Error: backward bracket cannot come before forward bracket\n");
+        else printf("All brackets are set correctly\n");
+    }
 }
