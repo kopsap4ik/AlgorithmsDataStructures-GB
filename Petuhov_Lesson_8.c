9c7062e44e6b6b218485f856649be70db3262c0c
Vasily Petuhov <vspetukhov@icloud.com>
2021-10-08 23:42:30 +0300

    done task_1

--- Petuhov_Lesson_8.c
+++ Petuhov_Lesson_8.c
@@ -60,9 +60,48 @@ void menu()
 
 // MARK: 1. Реализовать сортировку подсчетом.
 
+#define MaxArraySize 1000
+#define arrLenght 40
+
+int initialArray[arrLenght] = {667, 87, 76, 27, 33, 52, 429, 79, 16, 85, 12, 355, 55, 34, 293, 10, 404, 68, 69, 24, 960, 92, 42, 235, 36, 71, 84, 602, 12, 65, 338, 66, 31, 60, 41, 48, 44, 16, 37, 11};
+
+void countingSort(int* initialArray, int *sortedArray, int arrayLength) {
+    int tempArray[MaxArraySize];
+    
+    for (int i = 0; i < MaxArraySize + 1; i++) {
+        tempArray[i] = 0;
+    }
+    
+    for (int j = 0; j < arrayLength; j++) {
+        tempArray[initialArray[j]] = tempArray[initialArray[j]] + 1;
+    }
+    
+    for (int i = 1; i < MaxArraySize + 1; i++) {
+        tempArray[i] = tempArray[i] + tempArray[i - 1];
+    }
+    
+    for (int j = arrayLength - 1; j >= 0; j--) {
+        sortedArray[tempArray[initialArray[j]] - 1] = initialArray[j];
+        tempArray[initialArray[j]] = tempArray[initialArray[j]] - 1;
+    }
+}
+
 void solution1()
 {
-
+    int sortedArray[arrLenght];
+    
+    printf("Unsorted array:\n");
+    for (int i = 0; i < arrLenght; i++) {
+        printf("%i ", initialArray[i]);
+    }
+    
+    countingSort(initialArray, sortedArray, arrLenght);
+    
+    printf("\n\nSorted array:\n");
+    for (int i = 0; i < arrLenght; i++) {
+        printf("%i ", sortedArray[i]);
+    }
+    printf("\n");
 }
 
 
